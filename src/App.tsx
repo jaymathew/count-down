c3dcb6c255da8a5647bed340a2e22b6cf705974f
Jay Mathew <jaymathew25@gmail.com>
2020-06-01 16:29:49 -0500

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+	const launchDate = new Date('2020,6,11')
+
+	const [daysLeft, setDaysLeft] = useState("00")
+	const [hoursLeft, setHoursLeft] = useState("00")
+	const [minutesLeft, setMinutesLeft] = useState("00")
+	const [secondsLeft, setSecondsLeft] = useState("00")
+
+
+	useEffect(() => {
+
+		const id = setInterval(() => {
+			
+			const now = new Date()
+			const timeBetween =  launchDate.getTime() - now.getTime()
+
+			if (timeBetween > 0) {
+				//update count down
+				const days = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
+				const hours = Math.floor((timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+				const minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60))
+				const seconds = Math.floor((timeBetween % (1000 * 60)) / 1000)
+
+				setDaysLeft(days.toString())
+				setHoursLeft(hours.toString())
+				setMinutesLeft(minutes.toString())
+				setSecondsLeft(seconds.toString())
+
+			} else {
+				//clear interval, lanuch has happned.
+				clearInterval(id);
+			}
+		});
+		
+		return () => {
+			clearInterval(id);
+		};
+	})
+
+
+	return (
+		<div className="countDown-container">
+			<div className="countDown">
+				<p>Launching Soon...</p>
+				<div className="countDown-wrapper">
+					<TimerElements 
+						label="Day(s)" 
+						value={daysLeft} 
+					/>
+					<TimerElements 
+						label="Hour(s)" 
+						value={hoursLeft} 
+					/>
+					<TimerElements 
+						label="Minute(s)" 
+						value={minutesLeft} 
+					/>
+					<TimerElements 
+						label="Second(s)" 
+						value={secondsLeft} 
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+
+
+
+
+interface TimerElementsStyle {
+	label: string
+	value: string
+}
+
+const TimerElements: React.SFC<TimerElementsStyle> = ({ label, value })=> {
+
+	return (
+		<div className="countDown-elments">
+			<div>
+				<span>{value}</span>
+			</div>
+			<p>{label}</p>
+		</div>
+	)
+
+}
